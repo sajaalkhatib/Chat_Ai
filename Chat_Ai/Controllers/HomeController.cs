1388d7a4f134ac8c5ac4ce8df7dd9a7fec2fea1b
sjalk <sjalk@SAJA_ALKHATIB>
2026-05-04 11:39:52 +0300

    Add project files.

---
+++ Chat_Ai/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Chat_Ai.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat_Ai.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
